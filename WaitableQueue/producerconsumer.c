db276e75d2bcc00134c124a556053b341774c9e7
MCMXIII <42819807+MCMXIII@users.noreply.github.com>
2019-09-10 15:15:31 +0300

    Add files via upload

--- WaitableQueue/producerconsumer.c
+++ WaitableQueue/producerconsumer.c
@@ -6,13 +6,12 @@
 #include <unistd.h>
 #include <math.h>
 
-waitableQueue producerToConsumer;
-waitableQueue consumerToProducer;
-int producersAmount = 0;
-int consumersAmount = 0;
-char** outMessages;
-char** inMessages;
-
+typedef struct {
+	waitableQueue pTC;
+	waitableQueue cTP;
+	int amount;
+	char* message;
+} argsStruct;
 void* producer(void* arg)
 {
 	char* msg;
@@ -20,14 +19,14 @@ void* producer(void* arg)
 	int i = 0;
 	for(i = 0; i < x; i++)
 	{
-		printf("Producer enters message: %s", (char*)arg);
-		while(writeToQueue(producerToConsumer, arg) == -1){}
+		printf("Producer enters message: %s", ((argsStruct*)arg)->message);
+		while(writeToQueue(((argsStruct*)arg)->pTC, (void*)(((argsStruct*)arg)->message)) == -1){}
 	}
-	setFinished(producerToConsumer, producersAmount);
+	setFinished(((argsStruct*)arg)->pTC, ((argsStruct*)arg)->amount);
 	i = 0;
-	while((getAmount(consumerToProducer) > 0 || !getFinished(consumerToProducer))&&i < x)
+	while((getAmount(((argsStruct*)arg)->cTP) > 0 || !getFinished(((argsStruct*)arg)->cTP))&&i < x)
 	{
-		msg = (char*)(readFromQueue(consumerToProducer));
+		msg = (char*)(readFromQueue(((argsStruct*)arg)->cTP));
 		if(msg != NULL)
 		{
 			printf("	Message read by producer: %s", msg);
@@ -40,127 +39,105 @@ void* producer(void* arg)
 void* consumer(void* arg)
 {
 	char* msg;
-	msg = (char*)(readFromQueue(producerToConsumer));
+	msg = (char*)(readFromQueue(((argsStruct*)arg)->pTC));
 	while(msg != NULL)
 	{
 		printf("	Message read by consumer: %s", msg);
-		printf("Consumer enters message: %s", (char*)arg);
-		writeToQueue(consumerToProducer, arg);
-		msg = (char*)(readFromQueue(producerToConsumer));
+		printf("Consumer enters message: %s", ((argsStruct*)arg)->message);
+		writeToQueue(((argsStruct*)arg)->cTP, (void*)(((argsStruct*)arg)->message));
+		msg = (char*)(readFromQueue(((argsStruct*)arg)->pTC));
 	}
-	setFinished(consumerToProducer, consumersAmount);
+	setFinished(((argsStruct*)arg)->cTP, ((argsStruct*)arg)->amount);
 	return NULL;
 }
 
-pthread_t* createProducers(int size)
+pthread_t* create(int size, waitableQueue pTC, waitableQueue cTP, argsStruct*** args, unsigned int isProducers)
 {
 	pthread_t* arr;
+	argsStruct* a;
 	int i;
-	producersAmount = size;
 	arr = (pthread_t*)malloc(size*sizeof(pthread_t));
 	if(arr == NULL)
 	{
 		return;
 	}
-	outMessages = (char**)malloc(size*sizeof(char*));
-	if(outMessages == NULL)
+	*args = (argsStruct**)malloc(size*sizeof(argsStruct*));
+	if(args == NULL)
 	{
 		free(arr);
 		return;
 	}
 	for(i = 0; i < size; i++)
 	{
-		outMessages[i] = (char*)malloc(25*sizeof(char));
-		if(outMessages[i] == NULL)
+		a = (argsStruct*)malloc(sizeof(argsStruct));
+		if(a == NULL)
 		{
+			free(args);
 			free(arr);
-			free(outMessages);
 			return;
 		}
-		sprintf(outMessages[i], "Out message number %d\n", i);
-	}
-	for(i = 0; i < size; i++)
-	{
-		pthread_create(&(arr[i]), NULL, &producer, (void*)(outMessages[i]));
+		a->amount = size;
+		a->pTC = pTC;
+		a->cTP = cTP;
+		a->message = (char*)malloc(25*sizeof(char));
+		if(a->message == NULL)
+		{
+			free(args);
+			free(arr);
+			free(a);
+			return;
+		}
+		if(isProducers)
+		{
+			sprintf(a->message, "Out message number %d\n", i);
+			pthread_create(&(arr[i]), NULL, &producer, (void*)a);
+		}
+		else
+		{
+			sprintf(a->message, "In message number %d\n", i);
+			pthread_create(&(arr[i]), NULL, &consumer, (void*)a);
+		}
+		(*args)[i] = a;
 	}
 	return arr;
 }
-pthread_t* createConsumers(int size)
-{
-	pthread_t* arr;
-	int i;
-	consumersAmount = size;
-	arr = (pthread_t*)malloc(size*sizeof(pthread_t));
-	if(arr == NULL)
-	{
-		return;
-	}
-	inMessages = (char**)malloc(size*sizeof(char*));
-	if(inMessages == NULL)
-	{
-		free(arr);
-		return;
-	}
-	for(i = 0; i < size; i++)
-	{
-		inMessages[i] = (char*)malloc(25*sizeof(char));
-		if(inMessages[i] == NULL)
-		{
-			free(arr);
-			free(inMessages);
-			return;
-		}
-		sprintf(inMessages[i], "In message number %d\n", i);
-	}
-	for(i = 0; i < size; i++)
-	{
-		pthread_create(&(arr[i]), NULL, &consumer, (void*)(inMessages[i]));
-	}
-	return arr;
-}
-void destroyProducers(pthread_t* arr)
+void destroy(pthread_t* arr, argsStruct** args)
 {
 	int i;
+	int amount = 0;
 	if(arr != NULL)
 	{
 		free(arr);
 	}
-	if(outMessages != NULL)
+	if(args != NULL)
 	{
-		for(i = 0; i < producersAmount; i++)
+		if(args[0] != NULL)
 		{
-			if(outMessages[i] != NULL)
+			amount = args[0]->amount;
+		}
+		for(i = 0; i < amount; i++)
+		{
+			if(args[i] != NULL)
 			{
-				free(outMessages[i]);
+				if(args[i]->message != NULL)
+				{
+					free(args[i]->message);
+				}
+				free(args[i]);
 			}
 		}
-		free(outMessages);
-	}
-}
-void destroyConsumers(pthread_t* arr)
-{
-	int i;
-	if(arr != NULL)
-	{
-		free(arr);
-	}
-	if(inMessages != NULL)
-	{
-		for(i = 0; i < consumersAmount; i++)
-		{
-			if(inMessages[i] != NULL)
-			{
-				free(inMessages[i]);
-			}
-		}
-		free(inMessages);
+		free(args);
 	}
 }
 int main()
 {
 	int i;
+	waitableQueue producerToConsumer;
+	waitableQueue consumerToProducer;
 	pthread_t* producers;
 	pthread_t* consumers;
+	argsStruct** producersArgs;
+	argsStruct** consumersArgs;
 	srand(time(0));
 	producerToConsumer = queueCreate(10);
 	if(producerToConsumer == NULL)
@@ -173,31 +150,31 @@ int main()
 		destroyQueue(producerToConsumer);
 		return -1;
 	}
-	producers = createProducers(200);
+	producers = create(200, producerToConsumer, consumerToProducer, &producersArgs, 1);
 	if(producers == NULL)
 	{
 		destroyQueue(producerToConsumer);
 		destroyQueue(consumerToProducer);
 		return -1;
 	}
-	consumers = createConsumers(300);
+	consumers = create(300, producerToConsumer, consumerToProducer, &consumersArgs, 0);
 	if(consumers == NULL)
 	{
 		destroyQueue(producerToConsumer);
 		destroyQueue(consumerToProducer);
-		destroyProducers(producers);
+		destroy(producers, producersArgs);
 		return -1;
 	}
-	for(i = 0; i < producersAmount; i++)
+	for(i = 0; i < 200; i++)
 	{
 		pthread_join(producers[i], NULL);
 	}
-	for(i = 0; i < consumersAmount; i++)
+	for(i = 0; i < 300; i++)
 	{
 		pthread_join(consumers[i], NULL);
 	}
-	destroyProducers(producers);
-	destroyConsumers(consumers);
+	destroy(producers, producersArgs);
+	destroy(consumers, consumersArgs);
 	destroyQueue(producerToConsumer);
 	destroyQueue(consumerToProducer);
 	printf("Done!\n");
