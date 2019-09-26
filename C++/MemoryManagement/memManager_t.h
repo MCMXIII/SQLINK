fc5b61193adbf1691d0850b3752873bc71c832b0
MCMXIII <42819807+MCMXIII@users.noreply.github.com>
2019-09-26 16:20:52 +0300

    Add files via upload

--- C++/MemoryManagement/memManager_t.h
+++ C++/MemoryManagement/memManager_t.h
@@ -11,10 +11,10 @@ class memManager_t
 		bool isEmpty() const { return size == 0; }
 		unsigned int getSize() const { return size; }
 		unsigned int getCapacity() const { return capacity; }
-		virtual char* read(const void* buffer, unsigned int bytes) const;
-		virtual char* read(const void* buffer, const unsigned int pos, unsigned int bytes) const;
-		virtual int write(void* buffer, const char* bytes);
-		virtual char write(void* buffer, const unsigned int pos, const char* bytes);
+		virtual char* read(const void* buffer, unsigned int bytes) const { return 0; }
+		virtual char* read(const void* buffer, const unsigned int pos, unsigned int bytes) const { return 0; }
+		virtual int write(void* buffer, const char* bytes) { return 0; }
+		virtual char write(void* buffer, const unsigned int pos, const char* bytes) { return 0; }
 	protected:
 		virtual ~memManager_t() {}
 	private:
